beacbf679cf3f452a521ebfaab5f8ecdcad8bbed
Paweena27 <59030185@kmitl.ac.th>
2017-10-05 16:21:35 +0700

    W07P10

--- lab07/lab07/Program.cs
+++ lab07/lab07/Program.cs
@@ -9,4 +9,30 @@ namespace lab07
         {
         }
     }
+
+    class Person
+    {
+        public virtual void SayHi()
+        {
+            Console.WriteLine("Hi I'm a person");
+        }
+    }
+
+    class Student : Person
+    {
+        public override void SayHi()
+        {
+            Console.WriteLine("Hi I'm a student");
+        }
+    }
+
+    class Teacher : Person
+    {
+        public new void SayHi()
+        {
+            Console.WriteLine("Hi I'm a teacher");
+        }
+    }
+
 }
+
