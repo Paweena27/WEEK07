e9724000cbc886deb99164540589c33042590822
Paweena27 <59030185@kmitl.ac.th>
2017-10-05 18:18:53 +0700

    W07P13

--- lab07/lab07/Program.cs
+++ lab07/lab07/Program.cs
@@ -7,6 +7,38 @@ namespace lab07
     {
         static void Main(string[] args)
         {
+            Person p = new Professor();
+            p.SayHi();
+            Console.ReadKey();
         }
     }
+    class Person
+    {
+        public virtual void SayHi()
+        {
+            Console.WriteLine("Hi I'm a person");
+        }
+    }
+    class Student : Person
+    {
+        public override void SayHi()
+        {
+            Console.WriteLine("Hi I'm a student");
+        }
+    }
+    class Teacher : Person
+    {
+        public override void SayHi()
+        {
+            Console.WriteLine("Hi I'm a teacher");
+        }
+    }
+    class Professor : Teacher
+    {
+        public new void SayHi()
+        {
+            Console.WriteLine("Hi I'm a Professor");
+        }
+    }
+
 }
