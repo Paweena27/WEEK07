d33f92a325dbe1c7cac6b91e3f06cdd1b53ed057
Paweena27 <59030185@kmitl.ac.th>
2017-10-05 17:24:46 +0700

    W07P12

--- lab07/lab07/Program.cs
+++ lab07/lab07/Program.cs
@@ -7,6 +7,43 @@ namespace lab07
     {
         static void Main(string[] args)
         {
+            Student su = new Student();
+            try
+            {
+                su.StudentID = "59030185";
+                su.Salary = 315;
+                Console.WriteLine("StudentID : " + su.StudentID);
+                Console.WriteLine("Salary by day : " + su.Salary);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+    class Student
+    {
+        private string getStudentID;
+        private double getSalary;
+
+        public string StudentID
+        {
+            get { return getStudentID; }
+            set { getStudentID = value; }
+        }
+        public double Salary
+        {
+            get { return getSalary; }
+            set
+            {
+                if (value > 300 && value <= 450)
+                    getSalary = value;
+                else
+                    throw (new Exception("Error!!!! invalid Salary"));
+
+            }
         }
     }
 }
+
