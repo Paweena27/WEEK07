419db4b3768aff44e43344697fc07ccfc9136d31
Paweena27 <59030185@kmitl.ac.th>
2017-10-05 15:44:05 +0700

    W07P05

--- lab07/lab07/Program.cs
+++ lab07/lab07/Program.cs
@@ -5,8 +5,39 @@ namespace lab07
 {
     class Program
     {
+
         static void Main(string[] args)
         {
+            Teacher teacher = new Teacher("Tom", 350f);
+            // teacher work for 20Hr/month
+            Console.WriteLine("{0} charge = {1}", teacher.TypeName(),
+                teacher.CalculateCharge(20f));
+            Console.ReadLine();
         }
+
     }
+
+
+    class Teacher
+    {
+        // constructor (for initial private/protected variables)
+        public Teacher(string name, float billingRate)
+        {
+            this.name = name;
+            this.billingRate = billingRate;
+        }
+        // figure out the charge based on teacher's rate
+        public float CalculateCharge(float hours)
+        {
+            return (hours * billingRate);
+        }
+        // return the name of this type
+        public string TypeName()
+        {
+            return ("Teacher");
+        }
+        private string name;
+        protected float billingRate;
+    }
+
 }
